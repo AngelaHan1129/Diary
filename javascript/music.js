f7bf85cec7792b4fbfcd53717ee4956b68d196f2
garyfan1204 <a0965392628@gmail.com>
2024-04-09 14:49:44 +0800

    new manage.html, manage.css, emotional.html, emotional.css

--- javascript/music.js
+++ javascript/music.js
@@ -1,11 +1,75 @@
-var musicinfo = document.getElementById('musicinfo')
+var player = document.getElementById("musicinfo");
+var previous = document.getElementById("previous");
+var next = document.getElementById("next");
 
-document.getElementById('previous').addEventListener('click',function(event){
+previous.addEventListener("click", function(event){
     event.preventDefault();
-    musicinfo.style.transition = "transform 0.5s ease"
-    musicinfo.style.animation = "fade-in"
-    
-    // setTimeout(function(){
-    //     musicinfo.style.transform = "translate(100%)"
-    // },500)
-})
+    player.style.animation = "FadeOutLeft 1s"
+    previous.style.animation = "FadeOut 1s"
+    next.style.animation = "FadeOut 1s"
+    player.addEventListener('animationend', function() {
+        player.style.animation = "FadeInRight 1s"
+        previous.style.animation = "FadeIn 1s"
+        next.style.animation = "FadeIn 1s"
+    })
+});
+
+next.addEventListener("click", function(event){
+    event.preventDefault();
+    player.style.animation = "FadeOutRight 1s"
+    previous.style.animation = "FadeOut 1s"
+    next.style.animation = "FadeOut 1s"
+    player.addEventListener('animationend', function(){
+        player.style.animation = "FadeInLeft 1s"
+        previous.style.animation = "FadeIn 1s"
+        next.style.animation = "FadeIn 1s"
+    })
+});
+
+// var playlist = document.getElementById("playlist");
+// var playlistwrap = document.querySelector(".playlist-wrap");
+// var isMouseDown = false;
+// var startX, scrollLeft, initialPosition;
+
+// playlist.addEventListener('mousedown', function (e) {
+//     isMouseDown = true;
+//     startX = e.pageX;
+//     scrollLeft = playlist.scrollLeft;
+//     initialPosition = parseInt(playlistwrap.style.transform.replace('translateX(', '').replace('px)', ''));
+//     if (isNaN(initialPosition)) {
+//         initialPosition = 0;
+//     }
+//     playlist.style.cursor = 'grabbing';
+// });
+
+// playlist.addEventListener('mouseleave', function () {
+//     isMouseDown = false;
+//     playlist.style.cursor = 'grab';
+// });
+
+// playlist.addEventListener('mouseup', function () {
+//     isMouseDown = false;
+//     playlist.style.cursor = 'grab';
+// });
+
+// playlist.addEventListener('mousemove', function (e) {
+//     if (!isMouseDown) return;
+//     e.preventDefault();
+//     var x = e.pageX;
+//     var walk = (x - startX);
+//     var newPosition = initialPosition + scrollLeft - walk;
+//     var playlistWidth = playlist.offsetWidth;
+//     var itemsWidth = playlistwrap.offsetWidth;
+
+//     if (newPosition > 0) {
+//         newPosition = 0; // 到达第一个物件时禁止继续向左拖曳
+//     } else if (playlistWidth - itemsWidth > newPosition) {
+//         newPosition = playlistWidth - itemsWidth;
+//     }
+
+//     playlistwrap.style.transform = 'translateX(' + newPosition + 'px)';
+// });
+
+// playlist.addEventListener('selectstart', function (e) {
+//     e.preventDefault();
+// });
