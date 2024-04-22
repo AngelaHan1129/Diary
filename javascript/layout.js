b297ba51fb89cb9334b1f56e59f3fb76260643dc
garyfan1204 <a0965392628@gmail.com>
2024-04-22 22:09:32 +0800

    new layout.html, layout.js, layout.css, layout-m.css, index-m.css, treehole-m.css

---
+++ javascript/layout.js
@@ -0,0 +1,84 @@
+const pages = {
+    '/index.html': 'index.html',
+    '/login.html': 'login.html',
+    '/userinfo.html': 'userinfo.html',
+    '/changepwd.html': 'changepwd.html',
+    '/selectgender.html': 'selectgender.html',
+    '/treehole.html': 'treehole.html',
+    '/music.html': 'music.html',
+    '/diary.html': 'diary.html'
+}
+
+// const titles = {
+//     'index.html': '首頁',
+//     'login.html': '登入?註冊?',
+//     'userinfo.html': '用戶資訊',
+//     'changpwd.html': '變更密碼',
+//     'selectgender.html': '選擇性別',
+//     'treehole.html': '樹洞',
+//     'music.html': '音樂',
+//     'diary.html': '日記'
+// }
+
+const cssFiles = {
+    'index.html': ['css/layout.css', 'css/layout-m.css', 'css/index.css', 'css/index-m.css'],
+    'login.html': ['css/layout.css', 'css/layout-m.css', 'css/login.css'],
+    'userinfo.html': ['css/layout.css', 'css/layout-m.css', 'css/userinfo.css'],
+    'changepwd.html': ['css/layout.css', 'css/layout-m.css', 'css/userinfo.css'],
+    'selectgender.html': ['css/layout.css', 'css/layout-m.css', 'css/userinfo.css'],
+    'treehole.html': ['css/layout.css', 'css/layout-m.css', 'css/treehole.css', 'css/treehole-m.css'],
+    'music.html': ['css/layout.css', 'css/layout-m.css', 'css/music.css'],
+    'diary.html': ['css/layout.css', 'css/layout-m.css', 'css/diary.css']
+}
+
+const jsFiles = {
+    'login.html': 'javascript/login.js',
+    'music.html': 'javascript/music.js',
+    'diary.html': 'javascript/diary.js',
+    'userinfo.html': 'javascript/userinfo.js'
+}
+
+function layout(page) {
+    fetch('layout.html')
+        .then(response => response.text())
+        .then(html => {
+            document.body.innerHTML = html;
+            fetch(page)
+                .then(response => response.text())
+                .then(pageHtml => {
+                    const main = document.querySelector('main');
+                    main.innerHTML = pageHtml;
+                    // const title = titles[page]
+                    const cssFile = cssFiles[page];
+                    const jsFile = jsFiles[page];
+                    // if (title) {
+                    //     document.title = title;
+                    // }
+                    if (cssFile) {
+                        cssFile.forEach(cssfile => {
+                            const link = document.createElement('link')
+                            link.rel = 'stylesheet';
+                            link.href = cssfile;
+                            document.head.appendChild(link)
+                        });
+                    }
+                    if (jsFile) {
+                        const script = document.createElement('script');
+                        script.src = jsFile;
+                        document.body.appendChild(script);
+                    }
+                })
+                .catch(error => {
+                    console.error('加載指定頁面失敗：', error);
+                });
+        })
+        .catch(error => {
+            console.error('加載佈局失敗：', error);
+        });
+}
+
+window.addEventListener('load', function() {
+    const pathname = window.location.pathname;
+    let page = pages[pathname] || 'index.html';
+    layout(page);
+});
--- javascript/login.js
+++ javascript/login.js
@@ -1,112 +1,112 @@
 //不只以下mail 正則表達式
 function isValidEmail(email) {
-  var emailRegex = /.+@(gmail.com|yahoo.com.tw|live.com|mail.com)$/;
-  return emailRegex.test(email);
-}
-
-function validateRegister() {
-  var email = document.getElementById('email');
-  var passwd = document.getElementById('passwd');
-  var confirmrwd = document.getElementById('confirmpwd');
-
-  if (!isValidEmail(email.value)) {
-    alert('請輸入符合格式的電子信箱');
-    return false;
+    var emailRegex = /.+@(gmail.com|yahoo.com.tw|live.com|mail.com)$/;
+    return emailRegex.test(email);
   }
-
-  let password = passwd.value;
-  let checkpsw = confirmrwd.value;
-  if (password !== checkpsw) {
-    console.log(password);
-    alert('前後密碼不相符')
-    return false;
+  
+  function validateRegister() {
+    var email = document.getElementById('email');
+    var passwd = document.getElementById('passwd');
+    var confirmrwd = document.getElementById('confirmpwd');
+  
+    if (!isValidEmail(email.value)) {
+      alert('請輸入符合格式的電子信箱');
+      return false;
+    }
+  
+    let password = passwd.value;
+    let checkpsw = confirmrwd.value;
+    if (password !== checkpsw) {
+      console.log(password);
+      alert('前後密碼不相符')
+      return false;
+    }
+    return true;
   }
-  return true;
-}
-
-var loginform = document.getElementById('loginform')
-var registerfrom = document.getElementById('registerform')
-
-document.getElementById("registerlink").addEventListener("click", function (event) {
-  event.preventDefault();
-  loginform.style.transition = "transform 0.5s ease"
-  registerfrom.style.transition = "transform 0.5s ease"
-  loginform.style.transform = "translateX(-90%) rotate(15deg)"
-  registerfrom.style.transform = "rotate(0)"
-
-  setTimeout(function () {
-    loginform.style.zIndex = "0"
-    registerfrom.style.zIndex = "1"
-    loginform.style.transform = "translateX(0) rotate(15deg)";
-  }, 500);
-});
-
-document.getElementById("loginlink").addEventListener("click", function (event) {
-  event.preventDefault();
-  loginform.style.transition = "transform 0.5s ease"
-  registerfrom.style.transition = "transform 0.5s ease"
-  registerfrom.style.transform = "translateX(90%) rotate(15deg)"
-  loginform.style.transform = "rotate(0)"
-
-  setTimeout(function () {
-    registerfrom.style.zIndex = "0"
-    loginform.style.zIndex = "1"
-    registerfrom.style.transform = "translateX(0) rotate(15deg)";
-  }, 500);
-});
-
-
-async function loginData() {
-  const formLogin = await document.querySelector('.loginform');
-  let msg = '';
-  try {
-    formLogin.addEventListener('submit', event => {
-      event.preventDefault();
-      const formData = new FormData(formLogin);
-      let object = {};
-      formData.forEach((value, key) => object[key] = value);
-      let json = JSON.stringify(object);
-      fetch('http://localhost:8000/api/login', {
-        method: 'post',
-        body: json
-      }).then(res => res.json())
-        .then(data => {
-          console.log(data)
-          errmsg = msg.replace("",data.msg)
-          document.getElementById('loginmsg').innerText = errmsg;
+  
+  var loginform = document.getElementById('loginform')
+  var registerfrom = document.getElementById('registerform')
+  
+  document.getElementById("registerlink").addEventListener("click", function (event) {
+    event.preventDefault();
+    loginform.style.transition = "transform 0.5s ease"
+    registerfrom.style.transition = "transform 0.5s ease"
+    loginform.style.transform = "translateX(-90%) rotate(15deg)"
+    registerfrom.style.transform = "rotate(0)"
+  
+    setTimeout(function () {
+      loginform.style.zIndex = "0"
+      registerfrom.style.zIndex = "1"
+      loginform.style.transform = "translateX(0) rotate(15deg)";
+    }, 500);
+  });
+  
+  document.getElementById("loginlink").addEventListener("click", function (event) {
+    event.preventDefault();
+    loginform.style.transition = "transform 0.5s ease"
+    registerfrom.style.transition = "transform 0.5s ease"
+    registerfrom.style.transform = "translateX(90%) rotate(15deg)"
+    loginform.style.transform = "rotate(0)"
+  
+    setTimeout(function () {
+      registerfrom.style.zIndex = "0"
+      loginform.style.zIndex = "1"
+      registerfrom.style.transform = "translateX(0) rotate(15deg)";
+    }, 500);
+  });
+  
+  
+  async function loginData() {
+    const formLogin = await document.querySelector('.loginform');
+    let msg = '';
+    try {
+      formLogin.addEventListener('submit', event => {
+        event.preventDefault();
+        const formData = new FormData(formLogin);
+        let object = {};
+        formData.forEach((value, key) => object[key] = value);
+        let json = JSON.stringify(object);
+        fetch('http://localhost:8000/api/login', {
+          method: 'post',
+          body: json
+        }).then(res => res.json())
+          .then(data => {
+            console.log(data)
+            errmsg = msg.replace("",data.msg)
+            document.getElementById('loginmsg').innerText = errmsg;
+          })
+      });
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  loginData();
+  
+  async function registerData() {
+    const forRegister = await document.querySelector('.registerform');
+    let msg = '';
+    try {
+      forRegister.addEventListener('submit', event => {
+        event.preventDefault();
+        const formDataR = new FormData(forRegister);
+        let object = {};
+        formDataR.forEach((value, key) => object[key] = value);
+        let json = JSON.stringify(object);
+        fetch('http://localhost:8000/api/register', {
+          method: 'post',
+          body: json
         })
-    });
-  }
-  catch (err) {
-    console.log(err);
-  }
-}
-loginData();
-
-async function registerData() {
-  const forRegister = await document.querySelector('.registerform');
-  let msg = '';
-  try {
-    forRegister.addEventListener('submit', event => {
-      event.preventDefault();
-      const formDataR = new FormData(forRegister);
-      let object = {};
-      formDataR.forEach((value, key) => object[key] = value);
-      let json = JSON.stringify(object);
-      fetch('http://localhost:8000/api/register', {
-        method: 'post',
-        body: json
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            errmsg = msg.replace("",data.msg)
+            document.getElementById('.registermsg').innerHTML = errmsg;
+          })
       })
-        .then(res => res.json())
-        .then(data => {
-          console.log(data)
-          errmsg = msg.replace("",data.msg)
-          document.getElementById('.registermsg').innerHTML = errmsg;
-        })
-    })
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
-  catch (err) {
-    console.log(err)
-  }
-}
-registerData();
+  registerData();
